d8ea982551b5ea75e9005d780e03ac960dcc651a
Archiduke <manuxfull@gmail.com>
2021-10-05 13:40:34 +0200

    Cambios Cambios Cambios

--- js/js_index.js
+++ js/js_index.js
@@ -19,6 +19,16 @@
                 }
             }
 
+            document.querySelector('.boton').onmousemove = (e) => {
+
+                const x = e.pageX - e.target.offsetLeft
+                const y = e.pageY - e.target.offsetTop
+            
+                e.target.style.setProperty('--x', `${ x }px`)
+                e.target.style.setProperty('--y', `${ y }px`)
+                
+            }
+
             /* ahora hacemos la operacion elegida, mediante ifs */
 
             if(seleccion == "suma") { /* sumar */
